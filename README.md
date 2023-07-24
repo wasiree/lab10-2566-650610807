679d48e5f7e827779e32a2e539bb28b80000fead
Chayanin <arm.crazyplay@gmail.com>
2023-07-24 10:40:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lab 10 : Users Generator
+
+หลังจากการ Fork และ Clone repository แล้ว, ให้เปิดโฟลเดอร์ด้วย VSCode และรันคำสั่งใน terminal:
+
+```bash
+npm install
+npm run dev
+```
+
+ให้นักศึกษาสร้างหน้าเว็บ Users Generator โดยใช้ความรู้เรื่อง localStorage และการ useEffect hook ของ React
+
+[คลิกเพื่อดูรายละเอียด]()
+
+หลังจาก Deploy ด้วย Vercel แล้ว ให้แปะลิงค์ Deploy และลิงค์ GitHub Repo ของน.ศ. ไว้ใน Spreadsheet ด้านล่าง
+
+[คลิกเพื่อเปิด Spreadsheet]()
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { useState } from "react";
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
+import { UserCardDetail } from "./UserCardDetail";
+
+export const UserCard = ({ name, imgUrl, address, email }) => {
+  const [isDetailShown, setIsDetailShown] = useState(false);
+
+  const userCardOnClick = () => {
+    setIsDetailShown(!isDetailShown);
+  };
+
+  return (
+    <div className="border-bottom">
+      <div className="d-flex align-items-center p-3" onClick={userCardOnClick}>
+        <img src={imgUrl} width="90px" className="rounded-circle me-4"></img>
+        <span className="text-center display-6 me-auto">{name}</span>
+        {isDetailShown ? <IconChevronUp /> : <IconChevronDown />}
+      </div>
+      {isDetailShown && <UserCardDetail email={email} address={address} />}
+    </div>
+  );
+};
